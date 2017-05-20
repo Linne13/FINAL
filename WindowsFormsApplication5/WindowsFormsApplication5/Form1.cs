9fdf4aa68212fdd3bb16783d5bacfc1202993aac
unknown <a_khan@c441a-09.kbtu.kz>
2017-05-20 11:11:53 +0600

    11:10

--- WindowsFormsApplication5/WindowsFormsApplication5/Form1.cs
+++ WindowsFormsApplication5/WindowsFormsApplication5/Form1.cs
@@ -13,24 +13,29 @@ namespace WindowsFormsApplication5
     public partial class Form1 : Form
     {
 
-
+        Graphics g;
+        Point prevPoint;
+        List<Krug> krg;
+        public int cnt = 0;
 
         public Form1()
         {
             InitializeComponent();
+
+            g = CreateGraphics();
         }
 
-        Graphics g;
-
-        List<Krug> krg;
+        
 
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
-            krg = new List<Krug>();
-
-            krg.Add(new Krug(new Point(200,200), 90, 2));
+            //krg = new List<Krug>();
 
 
+
+            //krg.Add(new Krug(new Point(200,200), 0, 2));
+
+            //timer1.Enabled = true;
         }
 
         private void timer1_Tick(object sender, EventArgs e)
@@ -40,7 +45,29 @@ namespace WindowsFormsApplication5
             {
                 k.Move();
                 k.Draw(g);
+               
             }
+
+            
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+
+            
+            cnt++;
+            prevPoint = e.Location;
+
+            int minX = Math.Min(prevPoint.X, e.Location.X);
+            int minY = Math.Min(prevPoint.Y, e.Location.Y);
+            
+
+            krg = new List<Krug>();
+
+            krg.Add(new Krug(new Point(minX, minY), 0, 2));
+            
+
+            timer1.Enabled = true;
         }
     }
 }
