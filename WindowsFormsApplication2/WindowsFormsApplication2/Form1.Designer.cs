3bac4538a372a8c5d753d27ef81e467eb785c72e
unknown <a_khan@c441a-09.kbtu.kz>
2017-05-20 09:50:08 +0600

    9:50

--- WindowsFormsApplication2/WindowsFormsApplication2/Form1.Designer.cs
+++ WindowsFormsApplication2/WindowsFormsApplication2/Form1.Designer.cs
@@ -28,20 +28,38 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(-2, 1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(485, 460);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(484, 462);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(860, 635);
+            this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- WindowsFormsApplication2/WindowsFormsApplication2/Form1.cs
+++ WindowsFormsApplication2/WindowsFormsApplication2/Form1.cs
@@ -12,9 +12,50 @@ namespace WindowsFormsApplication2
 {
     public partial class Form1 : Form
     {
+
+        Graphics g;
+
+        int w;
+        int h;
+        Point prevPoint;
+        Pen pen = new Pen(Color.Blue);
+        
+
         public Form1()
         {
             InitializeComponent();
+            g = pictureBox1.CreateGraphics();
+        }
+
+        
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            prevPoint = e.Location;
+
+            
+            int minX = Math.Min(prevPoint.X, e.Location.X);
+            int minY = Math.Min(prevPoint.Y, e.Location.Y);
+
+            g.DrawEllipse(pen, minX, minY, 30, 30);
+            
+
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            prevPoint = e.Location;
+
+          
+            int minX = Math.Min(prevPoint.X, e.Location.X);
+            int minY = Math.Min(prevPoint.Y, e.Location.Y);
+
+            g.DrawEllipse(pen, minX, minY, 30, 30);
         }
     }
 }
